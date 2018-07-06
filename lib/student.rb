b00206d806dff91ba73c22ea76df193791931613
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-06 05:30:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,8 +23,10 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
-  def self.create(name, grade)
-    
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def save
