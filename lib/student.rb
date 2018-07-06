10788e2664d47ae3f5950b6328ba06d9c971a930
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-06 05:34:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,14 @@ attr_reader :id
     student
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
   def save
     if self.id
       self.update
