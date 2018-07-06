b906348f1139942b65e85faeb2189c5409c6b2b1
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-06 05:35:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,6 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize(name, grade, id=nil)
-    @id = id
-    @name = name
-    @grade = grade
-  end
 
   def self.create_table
     sql = <<-SQL
